63267106d44164a3103cf7db8cfc9636286d7c57
hgoelzer <heiko@goelzers.de>
2019-02-26 15:15:17 +0100

    updated netcdf attributes to pass the cf checker

--- toolbox/ncwrite_GrIS_retreatmasks.m
+++ toolbox/ncwrite_GrIS_retreatmasks.m
@@ -38,13 +38,14 @@ ncwriteatt(ancfile,'time', 'units', 'days since 1900-1-1 00:00:00') ;
 ncwriteatt(ancfile,'time', 'standard_name', 'time') ;
 ncwriteatt(ancfile,'time', 'calendar', 'gregorian') ;
 ncwriteatt(ancfile,'time', 'bounds', 'time_bounds') ;
-ncwriteatt(ancfile,'time', 'axis', 'time') ;
+ncwriteatt(ancfile,'time', 'axis', 'T') ;
 
+ncwriteatt(ancfile,'/','Conventions','CF-1.7')
 ncwriteatt(ancfile,'/','proj4','+init=epsg:3413')
 ncwriteatt(ancfile,'/','Description',['Retreat masks for outlet glacier forcing. Prepared for ISMIP6 by Heiko Goelzer (h.goelzer@uu.nl), IMAU, ', date ])
 % Add attributes to variables
-ncwriteatt(ancfile,varname, 'units', 'unitless') ;
-ncwriteatt(ancfile,varname, 'long_name', 'ice mask for retreat parameterisation') ;
+ncwriteatt(ancfile,varname, 'units', '1') ;
+ncwriteatt(ancfile,varname, 'long_name', 'ice fraction mask for retreat parameterisation') ;
 ncwriteatt(ancfile,varname, 'grid_mapping', 'mapping') ;
 %ncwriteatt(ancfile,varname, '_FillValue', -9999.) ; 
 
